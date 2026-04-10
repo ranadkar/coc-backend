f008cd98394f67d0da7d6aeb01fa44f888984028
Raj Nadkarni <ranadkar@ucsc.edu>
2026-04-10 14:54:31 -0700

    update tracked players

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ COC_API_BASE_URL = "https://api.clashofclans.com/v1"
 
 # Battle log polling config for the tracked players.
 TRACKED_BATTLE_LOG_TAGS = [
-    "PPOLJLR2Y",  # Austin
+    "PPOLJLR2Y", # Austin
+    "Q2RCJ9YLY", # fancy
     "PP2UOUPC",  # Bob
+    "22QOUU2VR", # space face
+    "YURLC99C8", # ruin
+    "8UQC99PJY", # quynh nhu
+    "LU8YGPL9J", # thanh diep
+    "YCYJU8PJO", # light
     "LU9VGCPUY", # Kevin
     "YVGCLY9U9", # BSE
     "GUQ22PC2L", # wuzhen
+    "QQUU9RGJR", # mheyding
+    "LOJ9JJYY8", # bananasux
+    "GYUUO8OVV", # yeowunnn
+    "98OCPPRG",  # starkiller
+    "ROJLOVQUO", # sun tzu
 ]
 BATTLE_LOG_SYNC_INTERVAL_SECONDS = 60 * 30
 BATTLE_LOG_CATEGORY_LIMIT = 50
